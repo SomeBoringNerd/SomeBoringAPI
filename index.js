eeda1052491036650d2caccac6fbe257f3d798cc
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2022-05-29 12:37:06 +0200

    added database stuff
    Also edited package.json for npm run install
    
    removed package-lock

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const needle = require('needle')
+const mysql = require('mysql');
 
 const { Webhook, Embed } = require('simple-discord-wh');
 
@@ -14,10 +15,26 @@ var GithubWebHook = require('express-github-webhook');
 
 var secret = config.github_secret;
 var webhook = config.webhook;
+var webhook_api = config.webhook_api;
+var webhook_loader = config.webhook_loader;
 var domain = config.domain;
 
+var con = mysql.createConnection({
+    host: config.db_host,
+    user: config.db_user,
+    password: config.db_password,
+    database: config.db_database
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 var webhookHandler = GithubWebHook({ path: '/api/hook/ttd', secret: secret });
-const hook = new Webhook(webhook)
+const hook_game = new Webhook(webhook)
+const hook_modding = new Webhook(webhook_api)
+const hook_loader = new Webhook(webhook_loader)
 
 const api = express();
 
@@ -28,22 +45,57 @@ let sub = "/api"
 
 webhookHandler.on('*', function (event, repo, data) {
 
-    let title_and_message = data['head_commit']['message'];
+    let title_and_message;
+
+    try
+    {
+        title_and_message = data['head_commit']['message']
+    }
+    catch
+    {
+        title_and_message = "No title given \n\n No description given"
+    }
+
     let splited = title_and_message.split('\n\n')
     let title = splited[0]
     splited[0] = '';
     let msg = splited.join("");
-    
-    const embed = new Embed()
-    .setTitle('New commit on ' + data['repository']['full_name'])
-    .setAuthor('SomeBoringAPI', 'https://cdn.discordapp.com/avatars/283205890474115072/6c7f9f422b07832ea61721dd8f5627db.webp', 'https://someboringnerd.xyz/api')
-    .addField('Title', title , true)
-    .addField('Description', msg , true)
-    .setColor('#00b0f4')
-    .setTimestamp();
 
-    hook.send(embed);
+    switch(data['repository']['full_name']){
+        case "SomeBoringNerd/throughthedark":
+            const hook_1 = new Embed()
+                .setTitle('New commit on ' + data['repository']['full_name'])
+                .setAuthor('SomeBoringAPI', 'https://cdn.discordapp.com/avatars/283205890474115072/6c7f9f422b07832ea61721dd8f5627db.webp', 'https://someboringnerd.xyz/api')
+                .addField('Title', title , true)
+                .addField('Description', msg , true)
+                .setColor('#00b0f4')
+                .setTimestamp();
 
+                hook_1.send(hook_game);
+            break;
+        case "UnityModdingGroup/ModTemplate":
+            const hook_2 = new Embed()
+                .setTitle('New commit on ' + data['repository']['full_name'])
+                .setAuthor('SomeBoringAPI', 'https://cdn.discordapp.com/avatars/283205890474115072/6c7f9f422b07832ea61721dd8f5627db.webp', 'https://someboringnerd.xyz/api')
+                .addField('Title', title , true)
+                .addField('Description', msg , true)
+                .setColor('#00b0f4')
+                .setTimestamp();
+
+                hook_2.send(hook_modding);
+            break;
+        case "UnityModdingGroup/UnityModLoader":
+            const hook_3 = new Embed()
+                .setTitle('New commit on ' + data['repository']['full_name'])
+                .setAuthor('SomeBoringAPI', 'https://cdn.discordapp.com/avatars/283205890474115072/6c7f9f422b07832ea61721dd8f5627db.webp', 'https://someboringnerd.xyz/api')
+                .addField('Title', title , true)
+                .addField('Description', msg , true)
+                .setColor('#00b0f4')
+                .setTimestamp();
+
+                hook_3.send(hook_loader);
+            break;
+    }
 
 });
 
@@ -52,8 +104,11 @@ api.listen(port, () => {
 })
 
 api.use(bodyParser.json()); // must use bodyParser in express
-api.use(webhookHandler);
 
+// in latest body-parser use like below.
+api.use(bodyParser.urlencoded({ extended: true }));
+
+api.use(webhookHandler);
 
 api.get(sub, (req, res) => {
 
@@ -61,8 +116,11 @@ api.get(sub, (req, res) => {
     txt += "<html>\n<head>\n<title>" + ip + "/api" + "</title>\n</head>\n"
     txt += '<h1>list of valid endpoints</h1>\n';
     
-    txt += '<a href="/api/through_the_dark/student?id=0">https://' + ip + '/api/through_the_dark/student?id=0</a>\n<br><br>\n'
+    txt += '<a href="/api/ttd/student?id=1">https://' + ip + '/api/ttd/student?id=1</a>\n<br><br>\n'
     txt += '<a href="/api/status?ip=4b4f.org">https://' + ip + '/api/status?ip=4b4f.org</a>\n<br><br>\n'
+    txt += '<a href="/api/ttd/download/win">https://' + ip + '/api/ttd/download/win</a>\n<br><br>\n'
+    txt += '<a href="/api/ttd/download/mac">https://' + ip + '/api/ttd/download/mac</a>\n<br><br>\n'
+    txt += '<a href="/api/ttd/download/nix">https://' + ip + '/api/ttd/download/nix</a>\n<br><br>\n'
 
     res.send(txt)
 })
@@ -95,7 +153,7 @@ api.get(sub + '/status', (req, res) =>
                 minecraft_status += r.body['info']['clean'][0] + "\n"
             }
 
-            minecraft_status += '<h3> values fetched from <a href="api.mcsrvstat.us">api.mcsrvstat.us</a></h3>\n</body>\n</html>'
+            minecraft_status += '<h3> values fetched from <a href="https://api.mcsrvstat.us">api.mcsrvstat.us</a></h3>\n</body>\n</html>'
 
             res.send(minecraft_status)
         })
@@ -103,138 +161,94 @@ api.get(sub + '/status', (req, res) =>
     }
 })
 
-api.get(sub + '/through_the_dark/student', (req, res) =>
+api.get(sub + '/ttd/student', (req, res) =>
 {
-    if(character_name[req.query.id] != undefined)
-    {
-        
-        
-        let json_student_obj = {
-            name: character_name[req.query.id],
-            gender: gender[req.query.id],
-            description: short_description[req.query.id],
-            opinion: opinion[req.query.id],
-            romance: romance[req.query.id],
-            location: location[req.query.id]
-        } 
+    let id_select = parseInt(req.query.id) + 1;
 
-        res.json(json_student_obj)
+    var sql = "SELECT * FROM character_list WHERE chr_id = " + id_select + ";";
+    console.log(id_select)
+    con.query(sql, function (err, result, field) {
+        if (err) throw console.log(err);
         
-    }else{
-        let max_id = character_name.length - 1;
-        res.send("the id entered (" + req.query.id + ") is bigger than the allowed value (" + max_id + ")")
-    }
+        
+        var sql2 = "SELECT * FROM character_list;";
+        con.query(sql2, function (err, results, field)
+        {
+            _max = results.length;
+                                    
+            if(id_select - 1 > _max || id_select <= 1)
+            {
+                
+                let json_obj = {
+                    max: results.length
+                }
+                res.json(json_obj)
+                
+            }else{
+                let json_student_obj = {
+                    name: result[0].name,
+                    gender: result[0].gender,
+                    description: result[0].description,
+                    personality: result[0].personality,
+                    opinion: result[0].opinion,
+                    romance: result[0].romance,
+                    location: result[0].location
+                }
+
+                res.json(json_student_obj)
+            }
+        })
+    });
 });
 
+api.get(sub + '/ttd/download/win', (req, res) => {
+    res.send('<html><head><meta http-equiv="refresh" content="0; url=https://someboringnerd.xyz/download/latest_win.zip"/></html></head>')
+})
 
-let character_name = [
-    "Diana",
-    "Muzuniji",
-    "Jukka",
-    "\"Max\"",
-    "Gabriel",
-    "???",
-    "??????",
-    "\"looking-for-the-truth\"",
-    "\"light-that-shine-upon-everything\"",
-    "\"the-spy-from-france\"",
-    "Giyomu",
-    "Charlotte",
-    "Himiko",
-]
+api.get(sub + '/ttd/download/mac', (req, res) => {
+    res.send('<html><head><meta http-equiv="refresh" content="0; url=https://someboringnerd.xyz/download/latest_mac.zip"/></html></head>')
+})
 
-let gender = [
-    "woman",
-    "woman",
-    "man",
-    "man",
-    "Unknown",
-    "Unknown",
-    "woman",
-    "Unknown",
-    "probably a man",
-    "probably a woman",
-    "man",
-    "woman",
-    "woman",
-]
+api.get(sub + '/ttd/download/nix', (req, res) => {
+    res.send('<html><head><meta http-equiv="refresh" content="0; url=https://someboringnerd.xyz/download/latest_nix.zip"/></html></head>')
+})
 
-let short_description = [
-    "oh wait, that's me!",
-    "A kind trans-woman. Came from Japan to live her transition in a more tolerant society",
-    "Your average buddy, quite laid-back but otherwise trustworthy and he's a bit nerdy",
-    "Look like a hippie, but surprisingly he's not.",
-    "they have around the same age as Diana, but they are not registered at school. The only reason they are allowed on school ground is because they personally know the principal and most teacher, meaning they are allowed to follow class if they want despite not being a registered student. They are not related to a teacher or the principal, they are just a friend of him.",
-    "???",
-    "???",
-    "the person behind the pseudonyme \"looking-for-the-truth\" is a person that contacted Diana in order to collaborate and find answer about the disappearences. They don't want to meet you for some reasons.",
-    "the person behind the pseudonyme \"light-that-shine-upon-everything\" is someone that take order from \"looking-for-the-truth\", and is kind of a spy. But for now, they don't have a lot of intel about the culprits.",
-    "the person behind the pseudonyme \"the-spy-from-france\" is someone that take order from \"looking-for-the-truth\", and is have the task to analyse any clue they can find. For now, they didn't find a lot.",
-    "Giyomu is the librarian of the school. He's a quiet, calm, collected man that can read people's feeling just by the look of their face, how they act, etc... He's also very cultured, and really nerdy.",
-    "Charlotte is the wife of Giyomu. She work at the police station as a field agent.",
-    "Himiko is the dead sister of Giyomu. She was investigating on dissapearences that occured in that exact town 18 years ago, and died seven days before Diana's birth. She would have been 36 this year"
-]
 
-let opinion = [
-    "Well, I'm the best !",
-    "\"i think that girl need a hug, she look sad\"",
-    "He's one of her neighbors so he really like to spend time with her",
-    "didn't meet before yet, new classmates to eachother but he trusts her",
-    "a fellow comrade that have the same goal",
-    "???",
-    "???",
-    "The answer to our questions",
-    "A cool girl with lots of brain power",
-    "She has yet to prove her worth.",
-    "\"That Diana girl is very smart. I'm sure she can help around\"",
-    "\"Diana is smart, but I'm sure she's not as smart as people like to say\" \n ||Fuck you Charlotte ?||",
-    "Giyomu discribe her as a smart and cool girl.",
-]
+api.get(sub + '/ttd/add/pre', (req, res) => {
+    let add_pre = "<style>textarea {resize: none;}</style>\n";
 
-let romance = [
-    "What do you think I am ? a narcissist ?",
-    "yes",
-    "yes",
-    "maybe",
-    "no",
-    "no",
-    "She's not yours.",
-    "no",
-    "no",
-    "no",
-    "He's 32, married, with two kids. That's not happening.",
-    "She's 30, married, with two kids. That's not happening.",
-    "NO.",
-]
+    add_pre += '<form method="POST" action="/api/ttd/add/post">\n'
+    add_pre += '<p>name</p>\n<input name="name" maxlength="254" size="32" autocomplete="off" required/>\n'
+    add_pre += '<p>gender</p>\n<input name="gender" maxlength="254" size="32" autocomplete="off" required/>\n'
+    add_pre += '<p>description</p>\n<textarea name="desc" maxlength="254" size="64" rows="5" cols="60" autocomplete="off" resize="none" required></textarea>\n'
+    add_pre += '<p>opinion</p>\n<input name="opinion" maxlength="254" size="128" autocomplete="off" required/>\n'
+    add_pre += '<p>romance</p>\n<input name="romance" maxlength="254" size="32" autocomplete="off" required/>\n'
+    add_pre += '<p>personality</p>\n<input name="personality" maxlength="254" size="32" autocomplete="off" required/>\n'
+    add_pre += '<p>location</p>\n<input name="location" maxlength="254" size="64" autocomplete="off" required/>\n'
+    add_pre += '<p>token</p>\n<input type="password" name="token" maxlength="32" required/>\n<br>\n<input type="submit"/>\n'
+    add_pre += '</form>'
 
-let personality = [
-    "\"Cool smart and lonely\" ||and a huge weeb||",
-    "\"Onee-san\"",
-    "Nerd.",
-    "Energetic",
-    "Mysterious, almost weird. Also, very friendly",
-    "Brutally honest",
-    "Cute",
-    "Mysterious and creepy at time, but otherwise really helpfull and trust-worthy",
-    "Smart, and don't talk a lot. They are good at analysing informations and notice patterns.",
-    "She's VERY arrogant, but otherwise cooperative",
-    "Smart, calm, and a giant nerd",
-    "A real bitch. At least she stand to her words.",
-    "cool and smart, according to Giyomu",
-]
+    res.send(add_pre)
+})
 
-let location = [
-    "anywhere you are",
-    "school",
-    "school",
-    "school",
-    "School, city",
-    "??????????",
-    "??????????",
-    "Unknown",
-    "\"A man on a mission is only where we need him to be\"",
-    "Mostly where we need her. Often on the field looking for clues.",
-    "Mostly in the library, can sometime be seen with his wife outside of the school",
-    "Mostly at the police station, can sometime be seen with her husband outside of the school",
-    "6 feet deep inside the town's graveyard. She's next to the entrance, on your left when you enter. There is flowers on her grave, but according to Gimoyu, he's not the one who replace them every week.",
-]
+api.post(sub + '/ttd/add/post', (req, res) => 
+{
+    let char_add = ""
+
+    if(req.body.token != config.chr_add_token)
+    {
+        char_add = "wrong password"   
+    }else
+    {
+        
+        var sql = "INSERT INTO character_list (name, gender, description, opinion, romance, personality, location) VALUES ?";
+        var values = [
+            [req.body.name,req.body.gender,req.body.desc,req.body.opinion,req.body.romance,req.body.personality,req.body.location]
+        ];
+        con.query(sql, [values], function (err, result) {
+            if (err) throw char_add = err;
+        });
+    }
+
+    res.send(char_add)
+})
